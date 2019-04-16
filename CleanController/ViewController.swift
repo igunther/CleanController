eceeee984bccc15c539c84ec54da0ad29ff2c639
Øystein Günther <developer@gunther.cc>
2019-04-16 12:22:19 +0200

    Initial Commit

---
+++ CleanController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanController
+//
+//  Created by Øystein Günther on 16/04/2019.
+//  Copyright © 2019 Øystein Günther. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
